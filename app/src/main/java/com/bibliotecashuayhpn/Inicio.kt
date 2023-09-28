b5d9c80a0ca8d2b536745738f39c3b3188759d5c
mante <juanfdelcastillo19@gmail.com>
2023-09-27 23:37:09 -0500

    Repositorio Inicial

---
+++ app/src/main/java/com/bibliotecashuayhpn/Inicio.kt
@@ -0,0 +1,12 @@
+package com.bibliotecashuayhpn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio)
+    }
+}
